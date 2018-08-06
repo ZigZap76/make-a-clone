4ba89ecacda0a620a6f72f05f0a7803ff3326dae
ZigZap76 <oussema.gargouri76@gmail.com>
2018-08-06 14:35:24 +0100

    gg

---
+++ Assets/ball.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ball : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private bool isPressed = false;
+    public float releaseTime = .15f;
+    public Rigidbody2D hook;
+    public float maxDragDistance = 3f;
+    public GameObject nextball ;
+    void Update()
+    {
+
+        if (isPressed)
+        {
+            Vector2 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector3.Distance(MousePos, hook.position) > maxDragDistance)
+                rb.position = hook.position +( MousePos - hook.position).normalized * maxDragDistance;
+            else
+                rb.position = MousePos;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+    void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+        StartCoroutine(Release());
+    }
+
+
+    IEnumerator Release()
+    {
+
+        yield return new WaitForSeconds(releaseTime);
+        GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
+
+        yield return new WaitForSeconds(2f);
+
+        if (nextball != null)
+        {
+            nextball.SetActive(true);
+        }
+
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+       
+        
+    }
+} 
---
+++ Assets/ennemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ennemy : MonoBehaviour {
+    public GameObject deathEffect;
+    public float health = 7f;
+    public static int EnemiesAlive = 0;
+
+     void Start()
+    {
+        EnemiesAlive++;
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log(col.relativeVelocity.magnitude);
+
+        if (col.relativeVelocity.magnitude > health)
+        {
+            Die();
+        }
+    }
+   void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        EnemiesAlive--;
+        if (EnemiesAlive <= 0)
+            Debug.Log("LEVEL WON");
+        Destroy(gameObject);
+    }
+}
